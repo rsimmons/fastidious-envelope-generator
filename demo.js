599bb082721ee87114d1c3ef3d4f73bc5acda9d6
Russel Simmons <symstym@mac.com>
2017-01-10 17:01:59 -0800

    Add graph of envelope generator output

--- demo.js
+++ demo.js
@@ -1,20 +1,48 @@
 'use strict';
 
-var EnvGen = require('./index.js');
+var EnvGen = require('./index');
+var createGrapher = require('./grapher');
 
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+// since ConstantSourceNode is not yet widely available
+function createConstantNode(audioContext, v) {
+  const constantBuffer = audioContext.createBuffer(1, 2, audioContext.sampleRate);
+  const constantData = constantBuffer.getChannelData(0);
+  constantData[0] = v;
+  constantData[1] = v;
+  const node = audioContext.createBufferSource();
+  node.buffer = constantBuffer;
+  node.loop = true;
+  node.start();
+  return node;
+}
+
+// Create nodes
 var oscNode = audioContext.createOscillator();
 oscNode.frequency.value = 440;
 oscNode.type = 'sine';
 oscNode.start();
 
-var gainNode = audioContext.createGain();
+var vcaNode = audioContext.createGain();
+vcaNode.gain.value = 0;
 
-var egen = new EnvGen(audioContext, gainNode.gain);
-oscNode.connect(gainNode);
-gainNode.connect(audioContext.destination);
+var constNode = createConstantNode(audioContext, 1);
 
+var envGainNode = audioContext.createGain();
+envGainNode.gain.value = 0;
+
+var grapherNode = createGrapher(audioContext, document.querySelector('#env-graph'), 1024);
+
+// Connect everything up
+oscNode.connect(vcaNode);
+vcaNode.connect(audioContext.destination);
+constNode.connect(envGainNode);
+var egen = new EnvGen(audioContext, envGainNode.gain);
+envGainNode.connect(grapherNode);
+grapherNode.connect(vcaNode.gain); // We have to insert it 'inline' otherwise it seems to get GC'd
+
+// Hook up controls
 var gateButtonElem = document.querySelector('#gate-button');
 gateButtonElem.addEventListener('mousedown', function(e) {
   e.preventDefault();
---
+++ grapher.js
@@ -0,0 +1,66 @@
+
+function createGrapher(audioContext, canvasElem, samplesPerPoint) {
+  var graphPoints = canvasElem.width;
+  var minY = 0;
+  var maxY = 1.0;
+
+  var graphDataMin = new Float32Array(graphPoints);
+  var graphDataMax = new Float32Array(graphPoints);
+  var graphDataStartIdx = 0;
+  var grapherNode = audioContext.createScriptProcessor(samplesPerPoint, 1, 1);
+  grapherNode.onaudioprocess = function(e) {
+    var inputData = e.inputBuffer.getChannelData(0);
+    var outputData = e.outputBuffer.getChannelData(0);
+
+    var minValue = Math.min.apply(null, inputData);
+    var maxValue = Math.max.apply(null, inputData);
+    graphDataMin[graphDataStartIdx] = minValue;
+    graphDataMax[graphDataStartIdx] = maxValue;
+    graphDataStartIdx++;
+    if (graphDataStartIdx === graphPoints) {
+      graphDataStartIdx = 0;
+    }
+
+    // Copy input to output unmodified
+    for (var i = 0; i < inputData.length; i++) {
+      outputData[i] = inputData[i];
+    }
+  };
+
+  var canvasCtx = canvasElem.getContext('2d');
+  var rafId;
+
+  function draw() {
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(0, 0, canvasElem.width, canvasElem.height);
+
+    canvasCtx.lineWidth = 1;
+    canvasCtx.strokeStyle = 'rgb(0, 255, 0)';
+
+    canvasCtx.beginPath();
+
+    for (var x = 0; x < graphPoints; x++) {
+      var idx = graphDataStartIdx+x;
+      if (idx >= graphPoints) {
+        idx -= graphPoints;
+      }
+
+      var y = canvasElem.height*(1 - (graphDataMax[idx]-minY)/(maxY-minY));
+
+      if (x === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+    }
+
+    canvasCtx.stroke();
+
+    rafId = requestAnimationFrame(draw);
+  };
+  draw();
+
+  return grapherNode;
+}
+
+module.exports = createGrapher;
