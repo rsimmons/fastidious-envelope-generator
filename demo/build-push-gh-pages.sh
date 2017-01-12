f3a6117984a77b859546eb658e1995e3c3a0d92c
Russel Simmons <symstym@mac.com>
2017-01-12 10:36:29 -0800

    Set up demo build system

---
+++ demo/build-push-gh-pages.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+REPO="git@github.com:rsimmons/fastidious-envelope-generator.git"
+
+# courtesy of http://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
+BUILDTMP=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+
+echo "Using tempdir $BUILDTMP ..."
+
+echo "Cloning from github ..."
+git clone --depth 1 $REPO $BUILDTMP/src
+
+SRC_SHA="$(cd $BUILDTMP/src && git rev-parse HEAD)"
+
+echo "Doing build ..."
+(
+cd $BUILDTMP/src
+cd demo
+yarn && yarn run build
+)
+
+echo "Cloning gh-pages from github ..."
+git clone --depth 1 --branch gh-pages $REPO $BUILDTMP/dst
+
+echo "Copying from build dir to gh-pages clone ..."
+rsync -a --delete --exclude '.git' $BUILDTMP/src/demo/build/ $BUILDTMP/dst
+
+echo "Commiting and pushing to origin gh-pages ..."
+(
+cd $BUILDTMP/dst
+git add --all
+git commit -m "build of $SRC_SHA"
+git push origin gh-pages:gh-pages
+)
+
+echo "Cleaning up temp dir ..."
+rm -rf $BUILDTMP
+
+echo "Done."
